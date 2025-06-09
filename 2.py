86ed079ac40e642edf528dd943a6e8cfe2bfbf98
Marcel <khabirov06@vk.com>
2025-06-09 22:29:23 +0300

    2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,8 @@
 def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x > pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x < pivot]
+    op = arr[len(arr) // 2]
+    left = [x for x in arr if x > op]
+    middle = [x for x in arr if x == op]
+    right = [x for x in arr if x < op]
     return quicksort(left) + middle + quicksort(right)
 
 numbers = [3, 6, 8, 7, 1, 2, 1]
