051828882a787400ece687985b0e5bdf512ab040
Marcel <khabirov06@vk.com>
2025-06-09 17:29:46 +0300

    Add files via upload

--- 5/5.py
+++ 5/5.py
@@ -1,1 +1,14 @@
+s1 = set(map(int, input("Введите первое множество через пробел: ").split()))
+s2 = set(map(int, input("Введите второе множество через пробел: ").split()))
 
+operation = input("Выберите операцию (1 - объединение, 2 - пересечение, 3 - разность): ")
+if operation == "1":
+    result = s1 | s2
+elif operation == "2":
+    result = s1 & s2
+elif operation == "3":
+    result = s1 - s2
+else:
+    result = "Произошла ошибка"
+
+print(result)
